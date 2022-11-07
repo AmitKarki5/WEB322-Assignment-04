5fb2e434ada59c8bf82dbcfe0816bccba8f8d835
AmitKarki5 <113572890+AmitKarki5@users.noreply.github.com>
2022-11-06 21:07:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//jshint esversion:6
 /*********************************************************************************
 *  WEB322 – Assignment 04
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
